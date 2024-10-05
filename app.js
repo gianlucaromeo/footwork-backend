4d41db598eab90cdb41ceef3421187b75a685c98
gianlucaromeo <gianlucaromeo@outlook.com>
2024-10-05 18:03:18 +0300

    Added mysql2 setup

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mysql = require('mysql2/promise')
 
-// const config = require('./utils/config')
-// const logger = require('./utils/logger')
-// TODO Add MySQL connection here and check connection
+// Connect to MySQL database
+mysql.createConnection(config.MYSQL_INFO)
+  .then((connection) => {
+    logger.info('Connected to MySQL database')
+    connection.end()
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MySQL database:', error)
+    process.exit(1)
+  })
 
+// Middleware setup
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
@@ -19,4 +29,5 @@ app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
+// Export the app
 module.exports = app
--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,16 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-// TODO Add database URI initialization here
+// TODO: Change the MYSQL_INFO object to use the environment variables
+const MYSQL_INFO = {
+  host: process.env.MYSQL_HOST_DEV,
+  user: process.env.MYSQL_USER_DEV,
+  password: process.env.MYSQL_PASSWORD_DEV,
+  database: process.env.MYSQL_DATABASE_DEV,
+  port: process.env.MYSQL_PORT_DEV
+}
 
 module.exports = {
   PORT,
-  // TODO Add export to database URI here
+  MYSQL_INFO
 }
