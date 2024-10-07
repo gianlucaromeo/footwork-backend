40a9be301bec67f46fbbace5ef394b01572a0cfa
gianlucaromeo <gianlucaromeo@outlook.com>
2024-10-07 14:41:15 +0300

    Added run_tests.sh script to run all the tests sequentially

---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run each test sequentially
+npm run test -- tests/students.test.js &&
+npm run test -- tests/admins.test.js &&
+npm run test -- tests/enrollments.test.js &&
+npm run test -- tests/courses.test.js &&
+npm run test -- tests/students.test.js
