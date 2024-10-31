2fbe5fc185bb414545536b31627ec10cdadcc8f5
gianlucaromeo <gianlucaromeo@outlook.com>
2024-10-31 14:16:02 +0200

    Refactored videos.test.js

--- tests/videos.test.js
+++ tests/videos.test.js
@@ -5,23 +5,12 @@ const app = require('../app')
 const supertest = require('supertest')
 const api = supertest(app)
 
-const helper = require('./tests_helper')
+const helper = require('./helper')
 
 const { sequelize } = require('../db/db')
 
-let firstAdminLoggedIn = null
-let firstStudentLoggedIn = null
-let secondStudentLoggedIn = null
-let initialVideos = null
-let initialChoreographies = null
-
 beforeEach(async () => {
-  const dbData = await helper.initizliaseDatabase()
-  firstAdminLoggedIn = dbData.firstAdminLoggedIn
-  firstStudentLoggedIn = dbData.firstStudentLoggedIn
-  initialVideos = dbData.initialVideos
-  secondStudentLoggedIn = dbData.secondStudentLoggedIn
-  initialChoreographies = dbData.initialChoreographies
+  await helper.initializeDatabase()
 })
 
 after(async () => {
@@ -30,39 +19,55 @@ after(async () => {
 
 describe('When there are initially some videos saved', () => {
   test('an admin can get all videos', async () => {
-    const response = await api
-      .get('/videos/admin/all')
-      .set('Authorization', `Bearer ${firstAdminLoggedIn.token}`)
+    const firstAdmin = helper.initialAdmins[0]
+
+    const firstAdminLoggedInResponse = await api
+      .post('/login/admin')
+      .send({ email: firstAdmin.email, password: firstAdmin.password })
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert(response.body.length === initialVideos.length)
+    const response = await api
+      .get('/videos/admin/all')
+      .set('Authorization', `Bearer ${firstAdminLoggedInResponse.body.token}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert(response.body.length === helper.initialVideos.length)
   })
 
   test('a student can get all the videos of the courses they are enrolled in', async () => {
+    const firstStudent = helper.initialStudents[0]
+
+    const firstStudentLoggedInResponse = await api
+      .post('/login/student')
+      .send({ email: firstStudent.email, password: firstStudent.password })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
     const response = await api
       .get('/videos/student/all')
-      .set('Authorization', `Bearer ${firstStudentLoggedIn.token}`)
+      .set('Authorization', `Bearer ${firstStudentLoggedInResponse.body.token}`)
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
     assert(response.body.length === 1)
-
-    const secondStudentVideosResponse = await api
-      .get('/videos/student/all')
-      .set('Authorization', `Bearer ${secondStudentLoggedIn.token}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    assert(secondStudentVideosResponse.body.length === 1)
   })
 })
 
-const path = require('path')
-const fs = require('fs')
-
 describe('Video Upload API', () => {
   test('an admin can upload a video and cover image', async () => {
+    const firstAdmin = helper.initialAdmins[0]
+
+    const firstAdminLoggedInResponse = await api
+      .post('/login/admin')
+      .send({ email: firstAdmin.email, password: firstAdmin.password })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const path = require('path')
+    const fs = require('fs')
+
     const videoFilePath = path.join(__dirname, 'test-video.mp4')
     const coverImagePath = path.join(__dirname, 'test-cover.png')
     const videoFile = fs.readFileSync(videoFilePath)
@@ -70,12 +75,18 @@ describe('Video Upload API', () => {
 
     const response = await api
       .post('/videos')
-      .set('Authorization', `Bearer ${firstAdminLoggedIn.token}`)
+      .set('Authorization', `Bearer ${firstAdminLoggedInResponse.body.token}`)
       .field('title', 'Test Video')
-      .field('choreographyId', initialChoreographies[0].id)
+      .field('choreographyId', helper.initialChoreographies[0].id)
       .field('folder', '/tests')
-      .attach('video', videoFile, { contentType: 'video/mp4', filename: 'test-video.mp4' })
-      .attach('coverImage', coverImageFile, { contentType: 'image/png', filename: 'test-cover.png' })
+      .attach('video', videoFile, {
+        contentType: 'video/mp4',
+        filename: 'test-video.mp4',
+      })
+      .attach('coverImage', coverImageFile, {
+        contentType: 'image/png',
+        filename: 'test-cover.png',
+      })
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
