214abef99b95e00c1822fcb22d86f6f8bf8c876c
gianlucaromeo <gianlucaromeo@outlook.com>
2024-10-31 13:33:42 +0200

    Refactored enrollments.test.js

--- tests/enrollments.test.js
+++ tests/enrollments.test.js
@@ -1,7 +1,7 @@
 const { beforeEach, after, describe, test } = require('node:test')
 const assert = require('assert')
 const { sequelize } = require('../db/db')
-const helper = require('./tests_helper')
+const helper = require('./helper')
 
 const Student = require('../models/student')
 const Course = require('../models/course')
@@ -11,13 +11,8 @@ const supertest = require('supertest')
 const Enrollment = require('../models/enrollment')
 const api = supertest(app)
 
-let firstAdminLoggedIn = null
-let initialEnrollments = null
-
 beforeEach(async () => {
-  const dbData = await helper.initizliaseDatabase()
-  initialEnrollments = dbData.initialEnrollments
-  firstAdminLoggedIn = dbData.firstAdminLoggedIn
+  await helper.initializeDatabase()
 })
 
 after(() => {
@@ -26,27 +21,41 @@ after(() => {
 
 describe('when there are initially some enrollments', () => {
   test('an admin can retrieve all the enrollments', async () => {
+    const firstAdmin = helper.initialAdmins[0]
+
+    const firstAdminLoggedInResponse = await api
+      .post('/login/admin')
+      .send({ email: firstAdmin.email, password: firstAdmin.password })
+      .expect(200)
+
     const response = await api
       .get('/enrollments')
-      .set('Authorization', `Bearer ${firstAdminLoggedIn.token}`)
+      .set('Authorization', `Bearer ${firstAdminLoggedInResponse.body.token}`)
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, initialEnrollments.length)
+    assert.strictEqual(response.body.length, helper.initialEnrollments.length)
   })
 
   test('an admin can create a new enrollment', async () => {
+    const firstAdmin = helper.initialAdmins[0]
+
+    const firstAdminLoggedInRepsonse = await api
+      .post('/login/admin')
+      .send({ email: firstAdmin.email, password: firstAdmin.password })
+      .expect(200)
+
     const students = await Student.findAll({ where: {} })
     const courses = await Course.findAll({ where: {} })
 
     const newEnrollment = {
-      studentId: students[1].id,
+      studentId: students[0].id,
       courseId: courses[1].id,
     }
 
     const response = await api
       .post('/enrollments')
-      .set('Authorization', `Bearer ${firstAdminLoggedIn.token}`)
+      .set('Authorization', `Bearer ${firstAdminLoggedInRepsonse.body.token}`)
       .send(newEnrollment)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -54,8 +63,8 @@ describe('when there are initially some enrollments', () => {
     assert.strictEqual(response.body.studentId, newEnrollment.studentId)
     assert.strictEqual(response.body.courseId, newEnrollment.courseId)
 
-    const enrollments = await Enrollment.findAll()
-    assert.strictEqual(enrollments.length, initialEnrollments.length + 1)
+    const enrollments = await Enrollment.findAll({})
+    assert.strictEqual(enrollments.length, helper.initialEnrollments.length + 1)
   })
 })
 
