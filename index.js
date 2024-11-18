ee19d4f7f689a515e8e6c98e99ef2c8bf3c93266
gianlucaromeo <gianlucaromeo@outlook.com>
2024-11-18 19:58:35 +0200

    Removed check

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
-  })
-}
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
